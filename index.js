a3a3fc6467695de579602ba4502b8bc69fc31f95
Dermot Cooney <cooneyde@tcd.ie>
2018-03-07 11:13:51 +0000

    set codec

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function convertAndSaveFileFFMPEG(inputPath) {
   return new Promise((resolve, reject) => {
 
     let process = new ffmpeg(inputPath)
-      .videoCodec('libx264')
+      .videoCodec('h264')
       .addOption('-threads', '1')
 
       .on('progress', function (info) {
